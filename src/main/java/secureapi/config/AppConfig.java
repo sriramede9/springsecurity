37ca55254c2539a3308b6f51e995834a1958f265
sriramede9 <only4programming03@gmail.com>
2019-06-26 17:47:40 -0400

    A basic skeleton that has no xml anywhere webapp using config files

---
+++ src/main/java/secureapi/config/AppConfig.java
@@ -0,0 +1,28 @@
+package secureapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "secureapi")
+
+public class AppConfig {
+
+	// define a bean for view resolver
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>HOME</title>
+</head>
+<body>
+	<h1>
+		<em> Sri It's time to look and feel things!!</em>
+	</h1>
+</body>
+</html>
