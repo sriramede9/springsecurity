b467c6e20ac81938f3150412c85cb6140e42e928
sriramede9 <only4programming03@gmail.com>
2019-07-11 17:57:01 -0400

    added jdbc properties file in class path and folowed spring security rules to load users and authories to authenticate users

--- src/main/java/secureapi/config/SecurityConfig.java
+++ src/main/java/secureapi/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package secureapi.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,16 +15,20 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		// add users for inmemory authentication
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("john").password("test").roles("EMPLOYEE"))
-				.withUser(users.username("mary").password("test").roles("EMPLOYEE", "MANAGER"))
-				.withUser(users.username("susan").password("test").roles("EMPLOYEE", "ADMIN"));
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//		auth.inMemoryAuthentication().withUser(users.username("john").password("test").roles("EMPLOYEE"))
+//				.withUser(users.username("mary").password("test").roles("EMPLOYEE", "MANAGER"))
+//				.withUser(users.username("susan").password("test").roles("EMPLOYEE", "ADMIN"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
